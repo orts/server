603a2363cb2bb5f36243190df0d4e30c9f8dca02
WibbenZ <wibbenz@gmail.com>
2015-03-21 21:15:26 +0100

    Update destroy field rune.lua
    Ref #31

--- data/spells/scripts/support/destroy field rune.lua
+++ data/spells/scripts/support/destroy field rune.lua
@@ -1,11 +1,13 @@
 function onCastSpell(creature, var, isHotkey)
 	local position = Variant.getPosition(var)
-	local tile = position:getTile()
-	local field = tile and tile:getItemByType(ITEM_TYPE_MAGICFIELD)
-	if field and isInArray(FIELDS, field.itemid) then
-		field:remove()
-		position:sendMagicEffect(CONST_ME_POFF)
-		return true
+	if position ~= Position(32824, 32243, 12) then
+		local tile = position:getTile()
+		local field = tile and tile:getItemByType(ITEM_TYPE_MAGICFIELD)
+		if field and isInArray(FIELDS, field.itemid) then
+			field:remove()
+			position:sendMagicEffect(CONST_ME_POFF)
+			return true
+		end
 	end
 
 	creature:sendCancelMessage(RETURNVALUE_NOTPOSSIBLE)
