1ae54b8a77ae1b582941a98aba0e7715db283bb8
Evil Puncker <gm.perflex@gmail.com>
2015-04-02 02:03:01 -0300

    Added knight guild leaders spells

--- data/npc/scripts/Thorwulf.lua
+++ data/npc/scripts/Thorwulf.lua
@@ -2,10 +2,10 @@ local keywordHandler = KeywordHandler:new()
 local npcHandler = NpcHandler:new(keywordHandler)
 NpcSystem.parseParameters(npcHandler)
 
-function onCreatureAppear(cid)          npcHandler:onCreatureAppear(cid)            end
-function onCreatureDisappear(cid)       npcHandler:onCreatureDisappear(cid)         end
-function onCreatureSay(cid, type, msg)      npcHandler:onCreatureSay(cid, type, msg)        end
-function onThink()              npcHandler:onThink()                    end
+function onCreatureAppear(cid)			npcHandler:onCreatureAppear(cid)			end
+function onCreatureDisappear(cid)		npcHandler:onCreatureDisappear(cid)			end
+function onCreatureSay(cid, type, msg)		npcHandler:onCreatureSay(cid, type, msg)		end
+function onThink()				npcHandler:onThink()					end
 
 local function addSpellKeyword(keyword, spellName, level, price)
 	local spellKeyword = keywordHandler:addKeyword(keyword, StdModule.say, {npcHandler = npcHandler, text = string.format("Do you want to learn the spell '%s' for %s?", spellName, price > 0 and price .. ' gold' or 'free')}, function(player) return player:isKnight() end)
