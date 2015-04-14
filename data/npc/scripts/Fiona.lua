af193d8de65be867b042e1cc300adb27002f847a
Evil Puncker <gm.perflex@gmail.com>
2015-04-14 16:51:40 -0300

    small fixes to Guide Elena

--- data/npc/scripts/Fiona.lua
+++ data/npc/scripts/Fiona.lua
@@ -5,7 +5,7 @@ NpcSystem.parseParameters(npcHandler)
 function onCreatureAppear(cid)			npcHandler:onCreatureAppear(cid)			end
 function onCreatureDisappear(cid)		npcHandler:onCreatureDisappear(cid)			end
 function onCreatureSay(cid, type, msg)		npcHandler:onCreatureSay(cid, type, msg)		end
-function onThink()		npcHandler:onThink()		end
+function onThink()				npcHandler:onThink()					end
 
 local voices = { {text = 'The Edron academy is always in need of magical ingredients!'} }
 npcHandler:addModule(VoiceModule:new(voices))
