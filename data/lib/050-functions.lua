519a9ff9d6ac303ad69209e800b5c3a8073b685c
Daniel Björkholm <danielbjorkholm@gmail.com>
2015-04-27 20:10:05 +0200

    Minor fixes 	Registered Rathleton doors in global.lua 	Added __index functions to global.lua 	Fixed #176, #177, and #178

--- data/lib/050-functions.lua
+++ data/lib/050-functions.lua
@@ -116,10 +116,6 @@ function Creature.getClosestFreePosition(self, position, extended)
 	return Position()
 end
 
-function Creature.getId(self)
-	return self.uid
-end
-
 function Creature.getMonster(self)
 	return self:isMonster() and self or nil
 end
@@ -261,24 +257,8 @@ if Game.getStorageValue("stamina") == -1 then
 end
 
 -- Item --
-function Item.getActionId(self)
-	return self.actionid
-end
-
-function Item.getId(self)
-	return self.itemid
-end
-
-function Item.getSubType(self)
-	return self.type
-end
-
 function Item.getType(self)
 	return ItemType(self.itemid)
-end
-
-function Item.getUniqueId(self)
-	return self.uid
 end
 
 function Item.isContainer(self)
