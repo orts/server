635c2384bb28099a0f676c065e65f727ad23ce18
Colandus <nabil_noobish@hotmail.com>
2016-12-18 02:10:38 +0100

    Update riddleTeleport.lua

--- data/movements/scripts/quests/dreamer challenge/riddleTeleport.lua
+++ data/movements/scripts/quests/dreamer challenge/riddleTeleport.lua
@@ -20,18 +20,17 @@ function onStepIn(player, item, position, fromPosition)
 		for x = -1, 1 do
 			for y = -1, 1 do
 				local item = Tile(pillowPos.center + Position(x, y, 0)):getThing(1)
-				if item and item.itemid == pillowPos.itemid then
-					pillows[#pillows + 1] = item
+				
+				-- Wrong pillow arrangement
+				if not item or item.itemid ~= pillowPos.itemid then
+					player:teleportTo(fromPosition, true)
+					fromPosition:sendMagicEffect(CONST_ME_TELEPORT)
+					return true
 				end
+				
+				pillows[#pillows + 1] = item
 			end
 		end
-	end
-
-	-- Wrong pillow arrangement
-	if #pillows < 36 then
-		player:teleportTo(fromPosition, true)
-		fromPosition:sendMagicEffect(CONST_ME_TELEPORT)
-		return true
 	end
 
 	player:teleportTo(destination)
